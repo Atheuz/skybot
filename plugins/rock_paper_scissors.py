f6e239672fa86f7b85204497c7f406aa4087ebdd
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2011-06-17 06:28:02 +0200

    Refactored outcome functions. Moved INSERT of row down. Using a dict to determine outcome now, considerably shorter than the other method. Removed chan key, as it's irrelevant, because there can only be a single registered nick on a network and network dbs are separate.

--- plugins/rock_paper_scissors.py
+++ plugins/rock_paper_scissors.py
@@ -6,108 +6,72 @@ from random import choice
 
 from util import hook
 
-def win(db, chan, nick):
-    db.execute("""INSERT or IGNORE INTO rockpaperscissors(
-               nick,
-               chan,
-               wins,
-               losses,
-               ties,
-               total) values(?,?,?,?,?,?)
-               """, (nick.lower(),chan.lower(),0,0,0,0))
+def win(db, nick):
     db.execute("""UPDATE rockpaperscissors SET
                wins = wins+1,
-               total = total+1
-               WHERE nick=? AND chan=?""", (nick.lower(), chan.lower()))
+               total = total+1 WHERE nick=nick""")
     db.commit()
 
-def loss(db, chan, nick):
-    db.execute("""INSERT or IGNORE INTO rockpaperscissors(
-               nick,
-               chan,
-               wins,
-               losses,
-               ties,
-               total) values(?,?,?,?,?,?)
-               """, (nick.lower(),chan.lower(),0,0,0,0))
+def loss(db, nick):
     db.execute("""UPDATE rockpaperscissors SET
                losses = losses+1,
-               total = total+1
-               WHERE nick=? AND chan=?""", (nick.lower(), chan.lower()))
+               total = total+1 WHERE nick=nick""")
     db.commit()
 
-def tie(db, chan, nick):
-    db.execute("""INSERT or IGNORE INTO rockpaperscissors(
-               nick,
-               chan,
-               wins,
-               losses,
-               ties,
-               total) values(?,?,?,?,?,?)
-               """, (nick.lower(),chan.lower(),0,0,0,0))
+def tie(db, nick):
     db.execute("""UPDATE rockpaperscissors SET
                ties = ties+1,
-               total = total+1
-               WHERE nick=? AND chan=?""", (nick.lower(), chan.lower()))
+               total = total+1 WHERE nick=nick""")
     db.commit()
 
-def get_stats(db, chan, nick):
-    return db.execute("""SELECT * FROM rockpaperscissors WHERE nick=? AND chan=?""", ( nick.lower(), chan.lower() ) ).fetchall()
+def get_stats(db, nick):
+    return db.execute("""SELECT * FROM rockpaperscissors WHERE nick=nick""").fetchall()
 
 @hook.command('rps')
 @hook.command()
-def rockpaperscissors(inp, nick='', chan='', db=None):
-    """.rps/.rockpaperscissors <hand>/<stats> -- plays rock-paper-scissors with
-    you or returns stats for all plays"""
+def rockpaperscissors(inp, nick='', db=None):
+    """.rps/.rockpaperscissors <hand>/<stats> -- plays rock-paper-scissors with you or returns stats for all plays"""
+
+    print nick
 
     stats = re.match('stats', inp)
     if stats:
-        out = get_stats(db, chan, nick)
+        out = get_stats(db, nick)
 
         if not out:
             return "no plays"
         else:
             return "you've won %s times, lost %s times, tied %s times and" \
-                   " played a total of %s times" % (out[0][2:])
-
-    hands = ['rock', 'paper', 'scissors']
+                   " played a total of %s times" % (out[0][1:])
 
     db.execute("""CREATE TABLE if not exists rockpaperscissors(
                nick TEXT PRIMARY KEY,
-               chan TEXT,
                wins INTEGER,
                losses INTEGER,
                ties INTEGER,
-               total INTEGER)
-               """)
+               total INTEGER)""")
+
+    hands = ['rock', 'paper', 'scissors']
+    hand_beats = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
 
     if inp and inp.lower() in hands:
+        db.execute("""INSERT or IGNORE INTO rockpaperscissors(
+                   nick,
+                   wins,
+                   losses,
+                   ties,
+                   total) values(?,?,?,?,?)""", (nick.lower(),0,0,0,0))
         player_hand = inp.lower()
         bot_hand = choice(hands)
 
         if player_hand == bot_hand:
-            tie(db, chan, nick)
+            tie(db, nick)
             return "%s - tie" % bot_hand
-        if player_hand == 'rock':
-            if bot_hand == 'paper':
-                loss(db, chan, nick)
-                return "%s - you lose" % bot_hand
-            if bot_hand == 'scissors':
-                win(db, chan, nick)
-                return "%s - you win" % bot_hand
-        if player_hand == 'paper':
-            if bot_hand == 'scissors':
-                loss(db, chan, nick)
-                return "%s - you lose" % bot_hand
-            if bot_hand == 'rock':
-                win(db, chan, nick)
-                return "%s - you win" % bot_hand
-        if player_hand == 'scissors':
-            if bot_hand == 'rock':
-                loss(db, chan, nick)
-                return "%s - you lose" % bot_hand
-            if bot_hand == 'paper':
-                win(db, chan, nick)
-                return "%s - you win" % bot_hand
-    else:
-        return
+        if hand_beats[player_hand] == bot_hand:
+            win(db, nick)
+            return "%s - you win" % bot_hand
+        else:
+            loss(db, nick)
+            return "%s - you lose" % bot_hand
+
+    return
